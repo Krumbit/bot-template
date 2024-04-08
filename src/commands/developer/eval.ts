c7cefbe8cbfda59acdd48bdf1bcf41e7158c1186
Krumbit <12988040+Krumbit@users.noreply.github.com>
2024-04-07 22:22:04 -0700

    fix(eval): unable to use js symbols

--- src/commands/developer/eval.ts
+++ src/commands/developer/eval.ts
@@ -1,42 +1,51 @@
-import util from 'util';
-import { Message, Client, EmbedBuilder } from "discord.js";
-
-import CommandHandler, { Category } from '@structures/CommandHandler';
+import CommandHandler, { Category } from "@structures/CommandHandler";
+import { Client, EmbedBuilder, Message } from "discord.js";
+import util from "util";
 
 export default class extends CommandHandler {
     constructor() {
         super({
             name: "eval",
             aliases: [],
-            description: "Allows our development team to evaluate provided code.",
-            usage: "<code>",
-            example: "message.channel.send(\"hello\")",
+            description: "Allows our development team to evalutate provided code.",
+            usage: "<code> [--s]",
+            example: 'message.channel.send("hello")',
             category: Category.developer,
-            devOnly: true
         });
     }
 
     async run(client: Client, message: Message<true>, args: string[]) {
         let code = args.join(" ");
-        if (!code.includes('return')) code = `return ${code}`;
 
-        const embed = new EmbedBuilder()
-            .setColor(client.colors.main);
+        const embed = new EmbedBuilder().setColor(client.colors.main);
 
         try {
             embed.setAuthor({ name: `Successful Evaluation` });
             embed.addFields({
                 name: `:outbox_tray: Output:`,
-                value: `\`\`\`js\n${util.inspect(await eval(`(async () => {${code}})()`)).split("").slice(0, 1000).join("")}\n\`\`\``
+                value: `\`\`\`js\n${util
+                    .inspect(await runCodeWithRunArgs(client, message, args, code))
+                    .split("")
+                    .slice(0, 1000)
+                    .join("")}\n\`\`\``,
             });
         } catch (e) {
             embed.setAuthor({ name: `Unsuccessful Evaluation` });
             embed.addFields({
                 name: `:outbox_tray: Error:`,
-                value: `\`\`\`js\n${e}\n\`\`\``
+                value: `\`\`\`js\n${e}\n\`\`\``,
             });
         }
 
         return message.post(embed);
     }
-};
+}
+
+async function runCodeWithRunArgs(
+    client: Client,
+    message: Message,
+    args: string[],
+    code: string,
+) {
+    return Function("client", "message", "args", code)(client, message, args);
+}
